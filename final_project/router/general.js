fa14bc060699124b370322353c783585fe90a7f3
g3ck00 <g3ck0@protonmail.ch>
2025-10-04 05:08:35 -0500

    Solve tasks (authentication code, GET for all books, GET for specified ISBN, GET for specified author)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,39 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const storedIsbn=req.params.isbn;
+  
+  const foundIsbn=books[storedIsbn];
+  
+  res.send(JSON.stringify(foundIsbn, null, 4));
+  
+  //I guess the keys are acting as ISBN...?
+  
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const booksAsArray=Object.values(books);
+  
+  const savedAuthor=req.params.author;
+  
+  let filteredAuthor=booksAsArray.filter((thor)=>thor.author===savedAuthor);
+  
+  //This code works, as long as the user inserts the author value with "%20" as space separators and watching the mayus/minus characters.
+  
+  //This code satifyes the task, but I think it has to be something else, like using just one keyword, not watching the mayus/minus characters, etc.
+  
+  //Please remember that in "thor.autor" it must always be "temporalVeriable.propertyInArray", not "temporalVariable.placeholderVaraible". I was doin' the last.
+  
+  res.send(JSON.stringify(filteredAuthor, null, 4));
+  
 });
 
 // Get all books based on title
