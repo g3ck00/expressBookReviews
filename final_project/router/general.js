a997f23d51ee2a5dcd743dba5eb1703b91b913c5
g3ck00 <g3ck0@protonmail.ch>
2025-10-06 05:31:19 -0500

    Add register function, solve "get all books based on title" task, "get book review" task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,44 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+	
+	const doesExist=(username) =>{
+	// Filter the users array for any user with the same username
+	let userswithsamename=users.filter((user) =>{
+	return user.username === username;
+	});
+	// Return true if any user with the same username is found, otherwise false
+	if(userswithsamename.length>0){
+		return true;
+	} else {
+		return false;
+	}
+	}
+
+	  
+	  const username=req.body.username; 
+	  const password=req.body.password;
+	  
+	  //check if both username and password are provided
+	  
+	  if (username && password){
+		  //check if it already exists
+		  if (!doesExist(username)){
+			  //adding in the users array
+			  users.push({"username":username, "password":password});
+			  
+			  return res.status(200).json({message: "User succesfully registered!"});
+		  } else {
+			  return res.status(404).json({message: "User already exists!"});
+		  }
+	  }
+	  
+	  //return error if username or password is missing
+	  return res.status(404).json({message:"Please enter a valid username and/or password..."});
+  
+	//register code working.
+  
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -31,11 +66,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   
-  const booksAsArray=Object.values(books);
+  const booksAsArray=Object.values(books); //converts books object to an array using Object.values(). this is for using the filter() method at it.
   
-  const savedAuthor=req.params.author;
+  const savedAuthor=req.params.author; //saves author placeholder into a variable (savedAuthor).
   
-  let filteredAuthor=booksAsArray.filter((thor)=>thor.author===savedAuthor);
+  let filteredAuthor=booksAsArray.filter((thor)=>thor.author===savedAuthor); //filters the results based in the placeholder entered by the user.
   
   //This code works, as long as the user inserts the author value with "%20" as space separators and watching the mayus/minus characters.
   
@@ -49,14 +84,32 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	
+	//same strategy of the previous task.
+	
+  const booksAsArray=Object.values(books);
+  
+  const storedTitle=req.params.title;
+  
+  let filtered=booksAsArray.filter((temporalTitle)=>temporalTitle.title===storedTitle);
+  
+  res.send(JSON.stringify(filtered, null, 4));
+  
+	//workin' great, but same observations like the previous task.
+  
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	
+	//Get ONLY reviews of a book using the ISBN
+	
+	const storedIsbn=req.params.isbn;
+  
+	const foundIsbn=books[storedIsbn].reviews;
+  
+	res.send(JSON.stringify(foundIsbn, null, 4));
+	
 });
 
 module.exports.general = public_users;
